b1d763e1a7391d7ad5318da7c9de0b1af169dd25
amc597 <74749920+amc597@users.noreply.github.com>
2024-08-13 14:48:34 -0500

    Create Add-SharedMailboxPermission.ps1

---
+++ Add-SharedMailboxPermission.ps1
@@ -0,0 +1,49 @@
+function Add-SharedMailboxPermission {
+    param (
+        $UsersWhoNeedAccess,
+        $AddUserToTheseEmails,
+        $AccessRights,
+        $SendAs
+    )
+    function Install-NeededPackages($PackageName, $MinimumVersion) {
+        foreach ($Package in $PackageName) {
+            if (!(Get-PackageProvider -ListAvailable | where Name -like "$Package")) {
+                Install-PackageProvider -Name $Package -MinimumVersion $MinimumVersion -Confirm:$false -Force
+                Write-Host -ForegroundColor Green "$Package has been installed."
+            }
+            elseif ((Get-PackageProvider -Name $Package).version -lt $MinimumVersion ) {
+                Install-PackageProvider -Name $Package -MinimumVersion $MinimumVersion -Confirm:$false -Force 
+                Write-Host -ForegroundColor Green "$Package has been installed."
+            }
+            else {
+                Write-Host -ForegroundColor Green "$Package found."
+            }
+        }
+    }
+
+    function Install-NeededModules($ModuleName) {
+        foreach ($Module in $ModuleName) {
+            if (!(Get-InstalledModule "$Module" -ErrorAction SilentlyContinue)) {
+                Install-Module $Module -Force -Confirm:$false
+                Write-Host -ForegroundColor Green "$Module has been installed."
+            }
+            else {
+                Write-Host -ForegroundColor Green "$Module found"
+            }
+        }
+    } 
+    $ModulesNeeded = "ExchangeOnlineManagement"
+    Install-NeededModules -ModuleName $ModulesNeeded   
+
+    Connect-ExchangeOnline
+
+    foreach($User in $UsersWhoNeedAccess){
+        foreach ($Email in $AddUserToTheseEmails) {
+            Add-MailboxPermission -Identity $Email -User $User -AccessRights $AccessRights 
+            Add-RecipientPermission -Identity $Email -AccessRights $SendAs -Trustee $User -Confirm:$false 
+            Write-Output "$User has $AccessRights and $SendAs rights to $Email"
+        }
+    }    
+}
+Add-SharedMailboxPermission -UsersWhoNeedAccess @("","") -AddUserToTheseEmails @("","") -AccessRights FullAccess -SendAs SendAs
+
